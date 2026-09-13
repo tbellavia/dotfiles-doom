ed50b6326fccb1cbe44920d3f2b4d1ac63a01247
tbellavia <bbellavi@student.42.fr>
2026-09-13 06:50:48 +0200

    add hackernews and multicursor package

--- packages.el
+++ packages.el
@@ -8,6 +8,7 @@
 ;;
 ;; Use 'C-h f package\!' to look up documentation for the `package!' macro.
 (package! tldr)
+(package! hackernews)
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;; (package! some-package)
